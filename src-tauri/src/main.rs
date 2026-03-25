a26ca47ff16a8e403aebafe5d0183b0bb93feb37
cxk4396 <1435100408@qq.com>
2026-03-25 13:47:53 +0800

    fix: initialize tauri plugins in main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,6 +8,10 @@ use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_sql::Builder::default().build())
         .setup(|app| {
             let app_handle = app.handle();
             tauri::async_runtime::spawn(async move {
